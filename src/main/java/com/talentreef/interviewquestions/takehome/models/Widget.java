d393c9c452f19c45db87e1f2eb52cdd69bed48da
unknown <jhodad@yahoo.com>
2024-04-14 20:06:17 -0600

    v0.1
    Added the constraints required to the Widgets' attributes

--- src/main/java/com/talentreef/interviewquestions/takehome/models/Widget.java
+++ src/main/java/com/talentreef/interviewquestions/takehome/models/Widget.java
@@ -8,7 +8,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 
 @Data
 @Table
@@ -16,9 +20,21 @@ import javax.persistence.Table;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
-@Builder(toBuilder=true)
+@Builder(toBuilder = true)
 public class Widget {
 
-  private String name;
+    @Id
+    private String id;
+
+    @Size(min = 3, max = 100, message ="Name must be between 3 and 100 characters")
+    private String name;
+
+    @Size(min = 5, max = 1000, message ="Description must be between 5 and 1000 characters")
+    private String description;
+
+    @DecimalMin("1.00")
+    @DecimalMax("20000.00")
+    @Digits(integer = 5, fraction = 2)
+    private double price;
 
 }
